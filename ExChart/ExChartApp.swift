6572725ec2d0e903adabf5396ed2d767a121c3e0
jake <palatable7@naver.com>
2023-10-31 09:10:53 +0900

    Implement

---
+++ ExChart/ExChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExChartApp.swift
+//  ExChart
+//
+//  Created by 김종권 on 2023/10/31.
+//
+
+import SwiftUI
+
+@main
+struct ExChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
